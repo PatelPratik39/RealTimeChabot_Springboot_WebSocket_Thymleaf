e2c2865f8e24b9bb05fd86bf7c85ab020d96d845
PatelPratik39 <ptl.pratik717@gmail.com>
2025-02-07 16:48:27 -0600

    Chat Controller file

--- springbootApp/src/main/java/com/chat/springbootApp/config/WebSocketConfig.java
+++ springbootApp/src/main/java/com/chat/springbootApp/config/WebSocketConfig.java
@@ -1,8 +1,28 @@
 package com.chat.springbootApp.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+       registry.addEndpoint("/chat")
+               .setAllowedOrigins("http://localhost:5173")
+               .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+//        set Message broker
+        registry.enableSimpleBroker("/topic");   //send messages to subscriber using /topic
+//        except message with /app/sendMessage
+        registry.setApplicationDestinationPrefixes("/app");
+
+    }
 }
---
+++ springbootApp/src/main/java/com/chat/springbootApp/controller/ChatController.java
@@ -0,0 +1,23 @@
+package com.chat.springbootApp.controller;
+
+import com.chat.springbootApp.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatController {
+
+    // /app/SendMessage
+
+    @MessageMapping("/app/sendMessage")
+    @SendTo("/topic/messages")
+    public ChatMessage sendMessage(ChatMessage message){
+        return message;
+    }
+    @GetMapping("chat")
+    public String chat(){
+        return "chat";
+    }
+}
